9db54daa8317d988e489c6685003072207879f70
chidee <chidiebereohiri@gmail.com>
2023-12-06 17:08:18 +0100

    Finished up with the transport model

--- transport/models.py
+++ transport/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from bus.models import Bus
 
 User = get_user_model()
 
-class RouteFromOshodi(models.Model):
+class Route(models.Model):
     route_choices = (
         (1, 'Oshodi'), 
         (2, 'Cele Exp.'), 
@@ -12,17 +13,22 @@ class RouteFromOshodi(models.Model):
         (5, 'Festac'), 
         (6, 'Ojota')
     )
-    pick_up = models.CharField(max_length=10, choices=route_choices, default='Oshodi')
+    pickup = models.CharField(max_length=10, choices=route_choices, default='Oshodi')
     destination = models.CharField(max_length=10, choices=route_choices)
     trip = models.CharField(
         max_length=20, 
         choices=(
             (1, 'One way'), 
             (2, 'Two way')
-        ), 
+        ),
         default='Two way'
     )
+    start_time = models.TimeField()
 
 
 class Booking(models.Model):
-    pass 
+    bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
+    passenger = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_verified = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+     
